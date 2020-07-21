b9b837de5392c35bfce0701ad9b9c38a2d36314d
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-07-21 18:29:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+default_phrase = "Squawk"
+
+phrase = ""
+
+def parrot(phrase)
+  puts word
+end
---
+++ runparrot.rb
@@ -0,0 +1,1 @@
+parrot("hi").parrot.rb
